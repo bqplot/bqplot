5e8793de824e0fe21f6d5ac390cc854368151c23
Dhruv Madeka <dmadeka1@bloomberg.net>
2015-02-05 16:15:06 +0000

    Update the docstring

--- bqplot/figure.py
+++ bqplot/figure.py
@@ -56,7 +56,7 @@ class Figure(DOMWidget):
     marks: List (default: [])
         list containing the marks which are to be appended to the figure
     overlay: any (default: )
-        Instance of the overlay for the figure
+        optional interaction layer for the figure
     scale_x: Scale
         Scale representing the x values of the figure
     scale_y: Scale
@@ -70,9 +70,12 @@ class Figure(DOMWidget):
         minimum height of the figure including the figure margins
     preserve_aspect: bool (default: False)
         Determines whether the aspect ratio for the figure specified by
-        min_width and min_height is preserved during resizing
+        min_width and min_height is preserved during resizing. This does not
+        guarantee that the data coordinates will have any specific aspect ratio.
     fig_margin: dict (default: {top=60, bottom=60, left=60, right=60})
-        dictionary containing the top, bottom, left and right margins
+        Dictionary containing the top, bottom, left and right margins. The user
+        is responsible for making sure that the width and height are greater
+        than the sum of the margins.
     padding_x: float (default: 0.0)
         Padding to be applied in pixels in the horizontal direction of the
         figure around the data points
@@ -82,71 +85,26 @@ class Figure(DOMWidget):
     legend_location:  {'top-right', 'top', 'top-left', 'left',
                        'bottom-left', 'bottom', 'bottom-right', 'right'}
         location of the legend relative to the center of the figure
-
-    HTML Attributes
-    ---------------
-    border_color
-
-    .. autosummary::
-
-       border_color
-       border_style
-       border_width
-
-       color
-       background_color
-       border_color
-
-       font_family
-       font_size
-       font_weight
-
-       width
-       height
-
-       padding
-       margin
     """
     _view_name = Unicode('bqplot.Figure', sync=True)
 
-    #: The title of the figure
     title = Unicode(sync=True,
                     exposed=True, display_index=1, display_name='Title')
-    #: List of axes
     axes = List(allow_none=False, sync=True)
-    #: List of marks
     marks = List(allow_none=False, sync=True)
-    #: The (optional) interaction layer widget
     overlay = Any(None, sync=True)
-    #: A scale instance for the horizontal global figure scale
     scale_x = Instance(Scale, sync=True)
-    #: A scale instance for the vertical global figure scale
     scale_y = Instance(Scale, sync=True)
 
-    #: Minimum width for the figure, including the figure margins
     min_width = CFloat(800.0, sync=True)
-    #: Minimum height for the figure, including the figure margins
     min_height = CFloat(600.0, sync=True)
-    #: Preserve the aspect ratio of the figure box specified with `min_width`
-    #: and `min_height`.
-    #: This does not guarantee that the data coordinates will have any specific
-    #: aspect ratio.
     preserve_aspect = Bool(False, sync=True, exposed=True, display_index=3,
                            display_name='Preserve aspect ratio')
 
-    #: Dictionary of figure margins, containing 'top', 'bottom', 'left' and
-    #: 'right' margins.
-    #: The user is responsible for making sure that the width and height are
-    #: greater than the sum of the margins.
     fig_margin = Dict(dict(top=60, bottom=60, left=60, right=60),
                       sync=True)
-    #: Padding to be applied around the data points in the figure in the
-    #: horizontal direction on both sides
     padding_x = Float(0.0, sync=True)
-    #: Padding to be applied around the data points in the figure in the
-    #: vertical direction on both sides
     padding_y = Float(0.025, sync=True)
-    #: The legend location
     legend_location = Enum(['top-right', 'top', 'top-left', 'left',
                            'bottom-left', 'bottom', 'bottom-right', 'right'],
                            default_value='top-right', allow_none=False,
