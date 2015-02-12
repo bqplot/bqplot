3a3201ff325ac3394ed03f369e70fab62f8f3efe
Srinivas Kumar Sunkara <ssunkara1@bloomberg.net>
2015-02-12 19:52:22 +0000

    fixing bug with flexline

--- bqplot/nbextension/LinesModel.js
+++ bqplot/nbextension/LinesModel.js
@@ -135,31 +135,49 @@ define(["widgets/js/manager", "d3", "./MarkModel"], function(WidgetManager, d3, 
 
     var FlexLineModel = LinesModel.extend({
         update_data:function() {
-            FlexLineModel.__super__.update_data.apply(this);
+            this.dirty = true;
+            // Handling data updates
+            var that = this;
+            this.x_data = this.get_typed_field("x");
+            this.y_data = this.get_typed_field("y");
+
             var scales = this.get("scales");
+            var x_scale = scales["x"], y_scale = scales["y"];
+            var curve_labels = this.get("labels");
+            if (this.x_data.length == 0 || this.y_data.length == 0) {
+                this.mark_data = [];
+            } else {
+                this.x_data = this.x_data[0] instanceof Array ?
+                    this.x_data : [this.x_data];
+                this.y_data = this.y_data[0] instanceof Array ?
+                    this.y_data : [this.y_data];
+                curve_labels = this.update_labels();
+
+                if (this.x_data.length == 1 && this.y_data.length > 1) {
+                    // same x for all y
+                    this.mark_data = curve_labels.map(function(name, i) {
+                        return {
+                            name: name,
+                            values: that.y_data[i].map(function(d, j) {
+                                return {x: that.x_data[0][j], y: d};
+                            }),
+                        };
+                    });
+                } else {
+                    this.mark_data = curve_labels.map(function(name, i) {
+                        var xy_data = d3.zip(that.x_data[i], that.y_data[i]);
+                        return {
+                            name: name,
+                            values: xy_data.map(function(d, j) {
+                                return {x: d[0], y: d[1]};
+                            }),
+                        };
+                    });
+                }
+            }
 
             var color_data = this.get_typed_field("color");
-            var x_data = this.get_typed_field("x");
             var width_data = this.get_typed_field("width");
-
-            var color_scale = scales["color"];
-            var width_scale = scales["width"];
-
-            if(color_scale && color_data.length > 0){
-                if(!this.get("preserve_domain")["color"]) {
-                    color_scale.compute_and_set_domain(color_data, this.id);
-                } else {
-                    color_scale.del_domain([], this.id);
-                }
-            }
-
-            if(width_scale && width_data.length > 0){
-                if(!this.get("preserve_domain")["width"]) {
-                    width_scale.compute_and_set_domain(width_data, this.id);
-                } else {
-                    width_scale.del_domain([], this.id);
-                }
-            }
 
             this.new_mark_data = this.mark_data.map(function(curve_elem) {
                 return {   name: curve_elem["name"],
@@ -174,7 +192,50 @@ define(["widgets/js/manager", "d3", "./MarkModel"], function(WidgetManager, d3, 
                            }),
                        };
             });
+            this.update_domains();
+            this.dirty = false;
             this.trigger("data_updated");
+        },
+        update_domains: function() {
+            var scales = this.get("scales");
+            var x_scale = scales["x"], y_scale = scales["y"];
+            var color_scale = scales["color"];
+            var width_scale = scales["width"];
+
+            if(!this.get("preserve_domain")["x"]) {
+                x_scale.compute_and_set_domain(this.mark_data.map(function(elem) {
+                    return elem.values.map(function(d) { return d.x; });
+                }), this.id);
+            } else {
+                x_scale.del_domain([], this.id);
+            }
+
+            if(!this.get("preserve_domain")["y"]) {
+                y_scale.compute_and_set_domain(this.mark_data.map(function(elem) {
+                    return elem.values.map(function(d) { return d.y; });
+                }), this.id);
+            } else {
+                y_scale.del_domain([], this.id);
+            }
+
+            if(color_scale !== null && color_scale !== undefined) {
+                if(!this.get("preserve_domain")["color"]) {
+                    color_scale.compute_and_set_domain(this.new_mark_data.map(function(elem) {
+                    return elem.values.map(function(d) { return d.color; })
+                    }), this.id);
+                } else {
+                    color_scale.del_domain([], this.id);
+                }
+            }
+            if(width_scale !== null && width_scale !== undefined) {
+                if(!this.get("preserve_domain")["width"]) {
+                    width_scale.compute_and_set_domain(this.new_mark_data.map(function(elem) {
+                    return elem.values.map(function(d) { return d.size; })
+                    }), this.id);
+                } else {
+                    width_scale.del_domain([], this.id);
+                }
+            }
         },
     });
     WidgetManager.WidgetManager.register_widget_model("bqplot.FlexLineModel", FlexLineModel);
