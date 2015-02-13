fcc67802a2cce92fb51cde9e3bd6b6ec15b26b9d
Romain Menegaux <rmenegaux@bloomberg.net>
2015-02-13 17:10:04 +0000

    new d3 symbols

---
+++ bqplot/nbextension/Markers.js
@@ -0,0 +1,88 @@
+/* Copyright 2015 Bloomberg Finance L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+define(["d3"], function(d3) {
+
+    var pi = Math.PI
+    var tan10 = Math.tan(10 * pi / 180);
+
+    var bqSymbolTypes = d3.map({
+        'arrow': function(size) {
+            var ry = Math.sqrt(size / tan10),
+                rx = ry * tan10;
+            return "M0," + -ry
+                + "L" + rx +"," + ry
+                + " " + -rx + "," + ry
+                + "Z";
+        },
+        'ellipsis': function(size, orientation) {
+            var slant = Math.pow(2, orientation)
+            var rx = Math.sqrt(size / (pi * slant));
+            var ry = rx * slant;
+            return "M0," + ry
+                + "A" + rx + "," + ry + " 0 1,1 0," + (-ry)
+                + "A" + rx + "," + ry + " 0 1,1 0," + ry
+                    + "Z";
+        }
+    });
+
+    function symbolSize() {
+        return 64;
+    }
+
+    function symbolType() {
+        return "arrow";
+    }
+
+    function symbolOrientation() {
+        return 0;
+    }
+
+
+    var bqSymbol = function() {
+        var type = symbolType,
+            size = symbolSize;
+            orientation = symbolOrientation;
+
+        function symbol(d,i) {
+            return bqSymbolTypes.get(type.call(this,d,i))
+                (size.call(this,d,i), orientation.call(this, d, i));
+        }
+
+        symbol.type = function(x) {
+            if (!arguments.length) return type;
+            type = d3.functor(x);
+            return symbol;
+        };
+
+        // size of symbol in square pixels
+        symbol.size = function(x) {
+            if (!arguments.length) return size;
+            size = d3.functor(x);
+            return symbol;
+        };
+
+        // orientation of symbol, in [-1,1]
+        symbol.orientation = function(x) {
+            if (!arguments.length) return orientation;
+            orientation = d3.functor(x);
+            return symbol;
+        };
+
+        return symbol;
+    };
+
+    return bqSymbol
+});
