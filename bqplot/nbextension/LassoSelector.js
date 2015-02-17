1c6768b15e56bf3f3de8e320e6e43a5441b337a2
Sylvain Corlay <scorlay@bloomberg.net>
2015-02-17 20:06:32 +0000

    foobar

--- bqplot/nbextension/LassoSelector.js
+++ bqplot/nbextension/LassoSelector.js
@@ -64,7 +64,6 @@ define(["./d3", "./Selector", "./utils", "./lasso_test"], function(d3, Selectors
             var lasso = this.el.append("path")
                 .attr("id", "l" + (++this.lasso_counter))
                 .on("click", function() {
-                    console.log("foobar");
                     //toggle the opacity of lassos
                     var lasso = d3.select(this);
                     lasso.classed("selected", !lasso.classed("selected"));
