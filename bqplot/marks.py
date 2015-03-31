792241bca35534b3f4e28001089e8dd01a94433d
Srinivas Kumar Sunkara <ssunkara1@bloomberg.net>
2015-03-31 16:21:19 +0000

    clean up of default custom msg sending code

--- bqplot/marks.py
+++ bqplot/marks.py
@@ -203,6 +203,7 @@ class Mark(Widget):
         self._hover_handlers = CallbackDispatcher()
         self._legend_click_handlers = CallbackDispatcher()
         self._element_click_handlers = CallbackDispatcher()
+        self._bg_click_handlers = CallbackDispatcher()
         self.on_msg(self._handle_custom_msgs)
 
     def on_hover(self, callback, remove=False):
@@ -214,6 +215,9 @@ class Mark(Widget):
     def on_element_click(self, callback, remove=False):
         self._element_click_handlers.register_callback(callback, remove=remove)
 
+    def on_background_click(self, callback, remove=False):
+        self._bg_click_handlers.register_callback(callback, remove=remove)
+
     def _handle_custom_msgs(self, _, content):
         if content.get('event', '') == 'hover':
             self._hover_handlers(self, content)
@@ -221,6 +225,8 @@ class Mark(Widget):
             self._legend_click_handlers(self, content)
         elif content.get('event', '') == 'element_click':
             self._element_click_handlers(self, content)
+        elif content.get('event', '') == 'background_click':
+            self._bg_click_handlers(self, content)
 
 
 @register_mark('bqplot.Lines')
