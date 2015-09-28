6216c048fa581dc3338e91b7e42b8a6dd02e4518
Sylvain Corlay <sylvain.corlay@gmail.com>
2015-09-28 22:36:00 +0100

    Include bower.json

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ class custom_sdist(sdist):
 
 setup_args = {
     'name': 'bqplot',
-    'version': '0.3.2',
+    'version': '0.3.3',
     'description': 'Interactive plotting for the Jupyter notebook, using d3.js and ipywidgets.',
     'License': 'Apache',
     'include_package_data': True,
