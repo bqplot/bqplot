78fa7570b21d4cdf3e93318229a41e77923715c7
Chakri Cherukuri <ccherukuri@bloomberg.net>
2015-10-28 14:05:32 +0000

    added MarkerLine mark

---
+++ bqplot/nbextension/MarkerLines.js
@@ -0,0 +1,160 @@
+/* Copyright 2015 Bloomberg Finance L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+define(["./components/d3/d3", "./Lines", "./Markers"], function(d3, LinesViewModule, markers) {
+    "use strict";
+
+    var bqSymbol = markers.symbol;
+    var MarkerLines = LinesViewModule.Lines.extend({
+        render: function() {
+            var base_render_promise = LinesViewModule.Lines.__super__.render.apply(this);
+            this.dot = bqSymbol().type(this.model.get("marker")).size(50);
+
+            var that = this;
+            return base_render_promise.then(function() {
+                that.event_listeners = {};
+                that.process_interactions();
+                that.create_listeners();
+                that.draw();
+            });
+        },
+        create_listeners: function() {
+            MarkerLines.__super__.create_listeners.apply(this);
+            this.listenTo(this.model, "change:marker", this.update_marker, this);
+        },
+        draw_legend: function(elem, x_disp, y_disp, inter_x_disp, inter_y_disp) {
+            var curve_labels = this.model.update_labels();
+            var legend_data = this.model.mark_data.map(function(d) {
+                return {index: d.index, name: d.name, color: d.color,
+                        opacity: d.opacity};
+            });
+            this.legend_el = elem.selectAll(".legend" + this.uuid)
+              .data(legend_data, function(d, i) {
+                  return d.name;
+              });
+
+            var that = this,
+                rect_dim = inter_y_disp * 0.8;
+
+            this.legend_el.enter()
+                .append("g")
+                .attr("class", "legend" + this.uuid)
+                .attr("transform", function(d, i) {
+                    return "translate(0, " + (i * inter_y_disp + y_disp)  + ")";
+                })
+                .on("mouseover", _.bind(function() {
+                    this.event_dispatcher("legend_mouse_over");
+                }, this))
+                .on("mouseout", _.bind(function() {
+                    this.event_dispatcher("legend_mouse_out");
+                }, this))
+                .on("click", _.bind(function() {
+                    this.event_dispatcher("legend_clicked");
+                }, this))
+                .each(function(d, i) {
+                    var g = d3.select(this);
+                    g.append("line")
+                        .style("stroke", function(d,i) { return that.get_colors(i); })
+                        .attr({x1: 0, x2: rect_dim, y1: rect_dim / 2 , y2: rect_dim / 2});
+                });
+
+           this.legend_el.append("text")
+               .attr("class", "legendtext")
+               .attr("x", rect_dim * 1.2)
+               .attr("y", rect_dim / 2)
+               .attr("dy", "0.35em")
+               .text(function(d, i) { return curve_labels[i]; })
+               .style("fill", function(d, i) { return that.get_element_color(d, i); })
+
+            var max_length = d3.max(curve_labels, function(d) {
+                return d.length;
+            });
+            this.legend_el.exit().remove();
+            return [this.model.mark_data.length, max_length];
+        },
+        draw: function(animate) {
+            var x_scale = this.scales.x, y_scale = this.scales.y;
+            this.set_ranges();
+            var animation_duration = animate === true ? this.parent.model.get("animation_duration") : 0;
+            this.line = d3.svg.line()
+                .x(function(d) { return x_scale.scale(d.x) + x_scale.offset; })
+                .y(function(d) { return y_scale.scale(d.y) + y_scale.offset; })
+                .defined(function(d) { return d.y !== null; });
+
+            var curves_sel = this.el.selectAll(".curve")
+                .data(this.model.mark_data, function(d, i) { return d.name; });
+
+            var new_curves = curves_sel.enter().append("g").attr("class", "curve");
+            new_curves.append("path").attr("class", "line").attr("fill", "none");
+            new_curves.append("g").attr("class", "dots");
+
+            var that = this;
+            curves_sel.select(".line")
+                .transition().duration(animation_duration)
+                .style("stroke", function(d, i) { return that.get_element_color(d, i); })
+                .style("stroke-width", this.model.get("stroke_width"))
+                .style("stroke-dasharray", _.bind(this.get_line_style, this))
+                .attr("d", function(d) { return that.line(d.values); });
+
+            var dots = curves_sel.select(".dots")
+                .selectAll("path")
+                .data(function(d) {
+                    return d.values.map(function(e, i) {
+                        return {x: e.x, y: e.y, color: that.get_colors(d.index)}; });
+                });
+
+            dots.enter().append("path");
+            dots.transition().duration(animation_duration)
+                .attr("transform", function(d) { return "translate(" + (x_scale.scale(d.x) + x_scale.offset) +
+                           "," + (y_scale.scale(d.y) + y_scale.offset) + ")";
+                })
+                .attr("d", this.dot)
+                .style("fill", function(d) { return d.color; });
+
+            //TODO: add drag listeners a la scatter
+            //dots.call(this.drag_listener);
+            dots.on("click", _.bind(function() {
+                this.event_dispatcher("element_clicked");
+            }, this));
+
+            dots.exit().remove();
+
+            this.x_pixels = this.model.mark_data.map(function(el) { return x_scale.scale(el.x) + x_scale.offset; });
+            this.y_pixels = this.model.mark_data.map(function(el) { return y_scale.scale(el.y) + y_scale.offset; });
+            this.apply_styles();
+
+            curves_sel.exit().remove();
+        },
+        update_marker: function(model, marker) {
+            if (!this.model.dirty) {
+                this.el.selectAll(".dots path").attr("d", this.dot.type(marker));
+
+            this.legend_el.select("path")
+                .attr("d", this.dot.type(marker));
+            }
+        },
+        relayout: function() {
+            this.set_ranges();
+            this.draw();
+        },
+        create_labels: function() {
+            //do nothing
+        },
+    });
+
+    return {
+        MarkerLines: MarkerLines,
+    };
+});
