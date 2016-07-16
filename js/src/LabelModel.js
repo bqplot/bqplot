f4cc2fb638ef0f608777c2a4f32f92616ab0f1a8
Dhruv Madeka <dmadeka1@bloomberg.net>
2016-07-16 18:10:43 -0400

    First cut of the multi-label

--- js/src/LabelModel.js
+++ js/src/LabelModel.js
@@ -23,22 +23,98 @@ var LabelModel = markmodel.MarkModel.extend({
         _model_name: "LabelModel",
         _view_name: "Label",
 
-        x: 0.0,
-        y: 0.0,
+        x: [],
+        y: [],
         x_offset: 0,
         y_offset: 0,
         scales_metadata: {
             x: { orientation: "horizontal", dimension: "x" },
             y: { orientation: "vertical", dimension: "y" },
-            color: { dimension: "color" }
+            color: { dimension: "colors" }
         },
-        color: null,
+        colors: null,
         rotate_angle: 0.0,
-        text: "",
+        text: [],
         font_size: "14px",
         font_weight: "bold",
         align: "start"
-    })
+    }),
+
+    initialize: function() {
+        // TODO: Normally, color, opacity and size should not require a redraw
+        LabelModel.__super__.initialize.apply(this);
+        this.on_some_change(["x", "y", "text"], this.update_data, this);
+        // FIXME: replace this with on("change:preserve_domain"). It is not done here because
+        // on_some_change depends on the GLOBAL backbone on("change") handler which
+        // is called AFTER the specific handlers on("change:foobar") and we make that
+        // assumption.
+        this.on_some_change(["preserve_domain"], this.update_domains, this);
+        this.update_data();
+        this.update_unique_ids();
+        this.update_domains();
+    },
+
+    update_data: function() {
+        this.dirty = true;
+        var x_data = this.get_typed_field("x"),
+            y_data = this.get_typed_field("y"),
+            text = this.get_typed_field("text"),
+            scales = this.get("scales"),
+            x_scale = scales.x,
+            y_scale = scales.y;
+
+        if (x_data.length === 0 || y_data.length === 0) {
+            this.mark_data = [];
+        } else {
+            //FIXME:Temporary fix to avoid misleading NaN error due to X and Y
+            //being of different lengths. In particular, if Y is of a smaller
+            //length, throws an error on the JS side
+            var min_len = Math.min(x_data.length, y_data.length);
+            x_data = x_data.slice(0, min_len);
+
+            this.mark_data = x_data.map(function(d, i) {
+                return {
+                    x: d,
+                    y: y_data[i],
+                    text: text[i],
+                };
+            });
+        }
+        this.update_unique_ids();
+        this.update_domains();
+        this.dirty = false;
+        this.trigger("data_updated");
+    },
+
+    update_unique_ids: function() {
+        this.mark_data.forEach(function(data, index){
+                                   data.unique_id = "Label" + index;
+        });
+    },
+
+    update_domains: function() {
+        if (!this.mark_data) {
+            return;
+        }
+        // color scale needs an issue in DateScaleModel to be fixed. It
+        // should be moved here as soon as that is fixed.
+
+       var scales = this.get("scales");
+       for (var key in scales) {
+            if(scales.hasOwnProperty(key) && key != "color") {
+                var scale = scales[key];
+                if(!this.get("preserve_domain")[key]) {
+                    scale.compute_and_set_domain(this.mark_data.map(function(elem) {
+                        return elem[key];
+                    }), this.id + key);
+                } else {
+                    scale.del_domain([], this.id + key);
+                }
+            }
+       }
+
+    },
+
 });
 
 module.exports = {
