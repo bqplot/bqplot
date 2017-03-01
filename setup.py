93a4d372e2cc2b0c8d95919d3dc90699dd2118b1
Jason Grout <jgrout6@bloomberg.net>
2017-03-01 16:12:02 -0500

    0.9.0b10
    Update widgets dependency

--- setup.py
+++ setup.py
@@ -181,7 +181,7 @@ setup_args = {
         ]),
     ],
     'install_requires': [
-        'ipywidgets>=6.0.0b9',
+        'ipywidgets>=6.0.0',
         'traitlets>=4.3.0',
         'traittypes>=0.0.6',
         'numpy>=1.10.4',
