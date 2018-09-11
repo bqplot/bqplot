ba945a6b5478f99ca66d700e5c89b34bdfc28bd7
Maarten Breddels <maartenbreddels@gmail.com>
2018-09-11 21:02:10 +0200

    add square marker, and make it fast (no texture)

--- js/shaders/scatter-fragment.glsl
+++ js/shaders/scatter-fragment.glsl
@@ -12,10 +12,11 @@ uniform sampler2D texture_previous;
 uniform float animation_time_texture;
 #endif
 
-// #define FAST_CIRCLE
+#define FAST_CIRCLE 1
+#define FAST_SQUARE 2
 
 void main(void) {
-    #ifdef FAST_CIRCLE
+    #if FAST_DRAW == FAST_CIRCLE
         float r = length(vUv - vec2(0.5))*2.;
         float radius = 0.7;
         float thickness = 0.1;
@@ -25,6 +26,9 @@ void main(void) {
         float alpha_stroke = smoothstep(0., overlap, ir) - alpha_fill;// * (1. - alpha_fill);
         gl_FragColor =  fill_color * alpha_fill + stroke_color * alpha_stroke;
         gl_FragColor.a = fill_color.a * alpha_fill + stroke_color.a *alpha_stroke;
+    #elif FAST_DRAW == FAST_SQUARE
+        gl_FragColor.rgb =  fill_color.rgb * fill_color.a;
+        gl_FragColor.a = fill_color.a;
     #else
         vec4 weights = texture2D(texture, vUv);
         // we use weights.r for the fill weight, and weights.g for the stroke weights
