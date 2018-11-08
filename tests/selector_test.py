44fa789ff325cbc5a85cd0e529756fd030b4e510
Maarten Breddels <maartenbreddels@gmail.com>
2018-11-08 14:38:28 +0100

    Fix/workaround for when nan's enter brush selector causing infinite loop

--- tests/selector_test.py
+++ tests/selector_test.py
@@ -1,5 +1,6 @@
 import bqplot
 from mock import Mock
+import numpy as np
 
 def test_brush_selector():
     selector = bqplot.interacts.BrushSelector()
@@ -40,4 +41,26 @@ def test_brush_selector():
     selector.selected_y = None
     assert selector.selected is None
     assert selector.selected_x is None
+    assert selector.selected_y is None
+
+    selector.selected = [[np.nan, 4], [5, 6]]
+    assert selector.selected_x is None
+    assert selector.selected_y.tolist() == [4, 6]
+
+    selector.selected = [[3, np.nan], [5, 6]]
+    assert selector.selected_x.tolist() == [3, 5]
+    assert selector.selected_y is None
+
+    selector.selected = [3, 4], [5, 6]
+    assert selector.selected_x.tolist() == [3, 5]
+    assert selector.selected_y.tolist() == [4, 6]
+    selector.selected_x = [3, np.nan]
+    assert selector.selected_x is None
+    assert selector.selected_y.tolist() == [4, 6]
+    selector.selected = [3, 4], [5, 6]
+    assert selector.selected_x.tolist() == [3, 5]
+    assert selector.selected_y.tolist() == [4, 6]
+
+    selector.selected_y = [4, np.nan]
+    assert selector.selected_x.tolist() == [3, 5]
     assert selector.selected_y is None
