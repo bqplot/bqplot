ea137f5fc9184a9d3f02afb3ba581b86c27e9662
martinRenou <martin.renou@gmail.com>
2019-04-16 00:48:57 +0200

    Fixing tests

--- js/test/scatter.ts
+++ js/test/scatter.ts
@@ -6,7 +6,7 @@ import bqplot = require('..');
 import {Figure} from '../src/Figure.js';
 import {FigureModel} from '../src/FigureModel.js';
 import {create_model, create_model_bqplot, create_view, create_figure_scatter} from './widget-utils'
-import * as d3 from 'd3';
+import * as d3Timer from 'd3-timer';
 
 
 describe("scatter >", () => {
@@ -15,53 +15,51 @@ describe("scatter >", () => {
     });
 
     it("create", async function() {
-        let x = {dtype: 'float32', value: new DataView((new Float32Array([0,1])).buffer)}
-        let y = {dtype: 'float32', value: new DataView((new Float32Array([2,3])).buffer)}
+        let x = {dtype: 'float32', value: new DataView((new Float32Array([0,1])).buffer)};
+        let y = {dtype: 'float32', value: new DataView((new Float32Array([2,3])).buffer)};
         let objects = await create_figure_scatter(this.manager, x, y);
         let scatter = objects.scatter;
-        let data = scatter.d3el.selectAll(".object_grp").data()
-        expect(data[0].x).to.equal(0)
-        expect(data[1].x).to.equal(1)
-        expect(data[0].y).to.equal(2)
-        expect(data[1].y).to.equal(3)
-        
-        d3.timer.flush() // this makes sure the animations are all executed
-        var transforms = scatter.d3el.selectAll(".object_grp")[0].map((el) => el.getAttribute('transform'));
+        let data = scatter.d3el.selectAll(".object_grp").data();
+        expect(data[0].x).to.equal(0);
+        expect(data[1].x).to.equal(1);
+        expect(data[0].y).to.equal(2);
+        expect(data[1].y).to.equal(3);
+
+        d3Timer.timerFlush(); // this makes sure the animations are all executed
+        var transforms = scatter.d3el.selectAll(".object_grp").nodes().map((el) => el.getAttribute('transform'));
         var width = objects.figure.plotarea_width;
         var height = objects.figure.plotarea_height;
-        expect(transforms).to.deep.equal([`translate(0,${height})`, `translate(${width},0)`])
-
+        expect(transforms).to.deep.equal([`translate(0, ${height})`, `translate(${width}, 0)`]);
     });
+
     it("computed fill", async function() {
         let x = [0, 1];
         let y = [2, 3];
         let objects = await create_figure_scatter(this.manager, x, y);
         let scatter = objects.scatter;
 
-        scatter.model.set('unselected_style', {'fill': 'orange', 'stroke': 'none'})
-        scatter.model.set('selected', [0])
-        let getFills = () => scatter.d3el.selectAll(".object_grp .element")[0].map((el) => ((el.getAttribute('style')||'').match(/fill: (\w*)/)||[null,null])[1]);
-        expect(getFills()).to.deep.equal([null, 'orange'])
+        scatter.model.set('unselected_style', {'fill': 'orange', 'stroke': 'none'});
+        scatter.model.set('selected', [0]);
+        let getFills = () => scatter.d3el.selectAll(".object_grp .element").nodes().map((el) => ((el.getAttribute('style')||'').match(/fill: (\w*)/)||[null,null])[1]);
+        expect(getFills()).to.deep.equal([null, 'orange']);
 
-        scatter.model.set('colors', ['red'])
-        expect(getFills()).to.deep.equal(['red', 'orange'])
+        scatter.model.set('colors', ['red']);
+        expect(getFills()).to.deep.equal(['red', 'orange']);
 
-        scatter.model.set('selected_style', {'fill': 'green', 'stroke': 'none'})
-        scatter.model.set('selected', [0])
-        expect(getFills()).to.deep.equal(['green', 'orange'])
+        scatter.model.set('selected_style', {'fill': 'green', 'stroke': 'none'});
+        scatter.model.set('selected', [0]);
+        expect(getFills()).to.deep.equal(['green', 'orange']);
 
-        scatter.model.set('selected', [1])
-        expect(getFills()).to.deep.equal(['orange', 'green'])
+        scatter.model.set('selected', [1]);
+        expect(getFills()).to.deep.equal(['orange', 'green']);
 
-        scatter.model.set('unselected_style', {})
-        expect(getFills()).to.deep.equal(['red', 'green'])
+        scatter.model.set('unselected_style', {});
+        expect(getFills()).to.deep.equal(['red', 'green']);
 
-        scatter.model.set('colors', [])
-        expect(getFills()).to.deep.equal([null, 'green'])
+        scatter.model.set('colors', []);
+        expect(getFills()).to.deep.equal([null, 'green']);
 
-        scatter.model.set('selected_style', {})
-        expect(getFills()).to.deep.equal([null, null])
-
-
+        scatter.model.set('selected_style', {});
+        expect(getFills()).to.deep.equal([null, null]);
     });
 });
--- js/test/utils.ts
+++ js/test/utils.ts
@@ -5,17 +5,17 @@ import {deepCopy, convert_dates} from '../src/utils.js';
 describe("utils >", () => {
     it("convert dates", async function() {
         let dates = ['2005-02-25', '2005-02-26', '2005-02-27'].map((x) => new Date(x));
-        let numbers = [1109289600000, 1109376000000, 1109462400000]
-        expect(Array.from(convert_dates(dates))).to.deep.equal(numbers)
-        expect([Array.from(convert_dates([dates])[0])]).to.deep.equal([numbers])
-        expect(Array.from(convert_dates(numbers))).to.deep.equal(numbers)
-        expect(Array.from(convert_dates([numbers, numbers]))).to.deep.equal([numbers, numbers])
+        let numbers = [1109289600000, 1109376000000, 1109462400000];
+        expect(Array.from(convert_dates(dates))).to.deep.equal(numbers);
+        expect([Array.from(convert_dates([dates])[0])]).to.deep.equal([numbers]);
+        expect(Array.from(convert_dates(numbers))).to.deep.equal(numbers);
+        expect(Array.from(convert_dates([numbers, numbers]))).to.deep.equal([numbers, numbers]);
     });
 
     it("deepCopy", function() {
         var ar = new Float64Array([0, 0.5]);
-        expect(Array.from(deepCopy(ar))).to.deep.equal(Array.from(ar))
-        expect(Array.from(deepCopy([ar])[0])).to.deep.equal(Array.from(ar))
-        expect(Array.from(deepCopy({ar:ar}).ar)).to.deep.equal(Array.from(ar))
+        expect(Array.from(deepCopy(ar))).to.deep.equal(Array.from(ar));
+        expect(Array.from(deepCopy([ar])[0])).to.deep.equal(Array.from(ar));
+        expect(Array.from(deepCopy({ar:ar}).ar)).to.deep.equal(Array.from(ar));
     })
 });
