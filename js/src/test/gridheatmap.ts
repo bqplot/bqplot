3478624012ac0e767b879ac62a54e6c42a15252e
martinRenou <martin.renou@gmail.com>
2020-01-16 14:03:10 +0100

    GridHeatmap: Fix selection
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- js/src/test/gridheatmap.ts
+++ js/src/test/gridheatmap.ts
@@ -41,4 +41,37 @@ describe("pie >", () => {
         expect(getFills(rects)).to.eql(expectedColors);
     });
 
+    it("update selected", async function() {
+        const colors = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+        const objects = await create_figure_gridheatmap(this.manager, colors);
+        const grid = objects.grid;
+
+        d3Timer.timerFlush();
+
+        grid.model.set('selected', [[0, 1], [1, 1], [2, 0]]);
+        grid.model.set('selected_style', {fill: 'red', opacity: 1})
+
+        d3Timer.timerFlush();
+
+        const rows = grid.d3el.selectAll('.heatmaprow');
+        const rects = rows.selectAll('rect');
+        let expectedColors = [
+            'rgb(165, 0, 38)', 'red', 'rgb(249, 142, 82)',
+            'rgb(254, 212, 129)', 'red', 'rgb(204, 234, 131)',
+            'red', 'rgb(45, 161, 85)', 'rgb(0, 104, 55)'];
+
+        expect(getFills(rects)).to.eql(expectedColors);
+
+        grid.model.set('selected', [[0, 2], [1, 1], [2, 1]]);
+
+        d3Timer.timerFlush();
+
+        expectedColors = [
+            'rgb(165, 0, 38)', 'rgb(222, 63, 46)', 'red',
+            'rgb(254, 212, 129)', 'red', 'rgb(204, 234, 131)',
+            'rgb(134, 203, 103)', 'red', 'rgb(0, 104, 55)'];
+
+        expect(getFills(rects)).to.eql(expectedColors);
+    });
+
 });
