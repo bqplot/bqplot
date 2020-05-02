70ad52d847a52aa675f1ad2c3155d08a2fcb2fb3
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-05-01 17:12:04 -0700

    Release 0.12.8

--- bqplot/_version.py
+++ bqplot/_version.py
@@ -1,8 +1,8 @@
-version_info = (0, 12, 7, 'final', 0)
+version_info = (0, 12, 8, 'final', 0)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
 __version__ = '%s.%s.%s%s'%(version_info[0], version_info[1], version_info[2],
   '' if version_info[3]=='final' else _specifier_[version_info[3]]+str(version_info[4]))
 
-__frontend_version__ = '^0.5.7'
+__frontend_version__ = '^0.5.8'
