24f3455944aa036f9d9da60db36140cd19570036
Maarten A. Breddels <maartenbreddels@gmail.com>
2021-02-22 14:20:14 +0100

    create new list instead of setting length

--- js/src/MarketMap.ts
+++ js/src/MarketMap.ts
@@ -226,9 +226,7 @@ export class MarketMap extends Figure {
 
     update_data() {
         const that = this;
-        const selected = this.model.get("selected");
-        selected.length = 0; 
-        this.model.set("selected", selected);
+        this.model.set("selected", []);
         this.data = this.model.get("names") || [];
         this.ref_data = this.model.get("ref_data");
         this.group_data = this.model.get("groups");
