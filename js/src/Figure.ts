0b49edefee44a3a24055e1572e5b177f3eb51bbe
martinRenou <martin.renou@gmail.com>
2021-09-24 16:33:40 +0200

    Speedup Do not relayout if not needed

--- js/src/Figure.ts
+++ js/src/Figure.ts
@@ -759,13 +759,7 @@ export class Figure extends DOMWidgetView {
       case 'after-show':
       case 'after-attach':
         if (this.pWidget.isVisible) {
-          const figureSize = this.getFigureSize();
-          if (
-            this.width !== figureSize.width ||
-            this.height !== figureSize.height
-          ) {
-            this.debouncedRelayout();
-          }
+          this.debouncedRelayout();
         }
         break;
     }
@@ -775,6 +769,12 @@ export class Figure extends DOMWidgetView {
     const relayoutImpl = () => {
       this.relayoutRequested = false; // reset relayout request
       const figureSize = this.getFigureSize();
+
+      if (this.width == figureSize.width && this.height == figureSize.height) {
+        // Bypass relayout
+        return;
+      }
+
       this.width = figureSize.width;
       this.height = figureSize.height;
       // update ranges
