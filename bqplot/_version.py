6d3ce1bbbf55906ab5a8ff6e9ab7572123fe49c0
martinRenou <martin.renou@gmail.com>
2022-01-07 13:36:24 +0100

    Release 0.12.32

--- bqplot/_version.py
+++ bqplot/_version.py
@@ -1,4 +1,4 @@
-version_info = (0, 12, 31, 'final', 0)
+version_info = (0, 12, 32, 'final', 0)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
@@ -6,4 +6,4 @@ __version__ = '%s.%s.%s%s' % (
     version_info[0], version_info[1], version_info[2],
     '' if version_info[3] == 'final' else _specifier_[version_info[3]] + str(version_info[4]))
 
-__frontend_version__ = '^0.5.32'
+__frontend_version__ = '^0.5.33'
