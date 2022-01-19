3142136cd57b3f8202fea49644c3d6020635480c
martinRenou <martin.renou@gmail.com>
2022-01-19 14:00:11 +0100

    Backport PR #1440: Fix CI

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path');
 
+const crypto = require('crypto');
+
+// Workaround for loaders using "md4" by default, which is not supported in FIPS-compliant OpenSSL
+const cryptoOrigCreateHash = crypto.createHash;
+crypto.createHash = (algorithm) =>
+  cryptoOrigCreateHash(algorithm == 'md4' ? 'sha256' : algorithm);
+
 const rules = [
   {
     test: /\.css$/,
