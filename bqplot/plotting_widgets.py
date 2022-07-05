5db3f56c8664d522b8316872b53bf5e2566fb135
Chakri Cherukuri <ccherukuri@bloomberg.net>
2022-07-05 12:53:43 -0400

    updates and renamed a few fields

--- bqplot/plotting_widgets.py
+++ bqplot/plotting_widgets.py
@@ -1,5 +1,5 @@
 from ipywidgets import Layout
-from traitlets import List, Enum, Int
+from traitlets import List, Enum, Int, Bool
 from traittypes import DataFrame
 from bqplot import Figure, LinearScale, Lines, Label
 from bqplot.marks import CATEGORY10
@@ -8,57 +8,49 @@ import numpy as np
 
 class Radar(Figure):
     """
-    Radar chart created from a pandas DataFrame. Each column of the df will be
+    Radar chart created from a pandas Dataframe. Each column of the df will be
     represented as a loop in the radar chart. Each row of the df will be
-    reprsented as a spoke of the radar chart.
+    represented as a spoke of the radar chart
 
     Attributes
     ----------
     data: DataFrame
-        Data for the radar
-    type: {"circle", "polygon"} (default: "circle")
-        Type of bands to in the radar
-    colors: List (default: CATEGORY10])
-        List of colors for the loops of the radar
+        data for the radar
+    band_type: {"circle", "polygon"} (default: "circle")
+        type of bands to display in the radar
     num_bands: Int (default: 5)
-        Number of bands on the radar. As of now, this attribute is not
-        dynamic and it has to set in the cinstructor
+        number of bands on the radar. As of now, this attribute is not
+        dynamic and it has to set in the constructor
     data_range: List (default: [0, 1])
-        Data to be mapped to the center and the perimeter of the radar
+        range of data
+    fill: Bool(default: True)
+        flag which lets us fill the radar loops or not
     """
 
     data = DataFrame()
     data_range = List([0, 1]).tag(sync=True)
-    type = Enum(
+    band_type = Enum(
         ["circle", "polygon"], default_value="circle", allow_none=True
     ).tag(sync=True)
     colors = List(default_value=CATEGORY10).tag(sync=True)
     num_bands = Int(default_value=5).tag(sync=True)
+    fill = Bool(default_value=False).tag(sync=True)
 
     def __init__(self, **kwargs):
         super(Radar, self).__init__(**kwargs)
-
         self.scales = {"x": LinearScale(), "y": LinearScale()}
         # set some defaults for the figure
         self.layout = Layout(min_width="600px", min_height="600px")
         self.max_aspect_ratio = 1
-        self.padding_y = 0
         self.preserve_aspect = True
 
-        # handlers for data updates
-        self.observe(self.update_data, "data")
-        self.observe(self.update_bands, ["type", "num_bands"])
+        # marks for the radar figure
 
-        # create marks
-        # spokes (straight lines going away from origin)
-        self.spokes = Lines(
-            scales=self.scales, colors=["#ccc"], stroke_width=0.5
-        )
+        # spokes (straight lines going away from the center)
+        self.spokes = Lines(scales=self.scales, colors=["#ccc"], stroke_width=0.5)
 
         # bands
-        self.bands = Lines(
-            colors=["#ccc"], scales=self.scales, stroke_width=0.5
-        )
+        self.bands = Lines(colors=["#ccc"], scales=self.scales, stroke_width=0.3,)
 
         # loops of the radar
         self.loops = Lines(
@@ -66,7 +58,7 @@ class Radar(Figure):
             display_legend=True,
             colors=self.colors,
             stroke_width=2,
-            fill="inside",
+            fill="inside" if self.fill else "none",
             marker="circle",
             marker_size=50,
         )
@@ -75,8 +67,17 @@ class Radar(Figure):
             scales=self.scales,
             default_size=12,
             font_weight="normal",
-            colors=["black"],
             apply_clip=False,
+            colors=["#ccc"],
+            align="middle",
+        )
+
+        self.spoke_labels = Label(
+            scales=self.scales,
+            default_size=14,
+            font_weight="bold",
+            apply_clip=False,
+            colors=["#ccc"],
             align="middle",
         )
 
@@ -85,7 +86,16 @@ class Radar(Figure):
             self.bands,
             self.loops,
             self.band_labels,
+            self.spoke_labels,
         ]
+
+        # handlers for data updates
+        self.observe(self.update_data, "data")
+        self.observe(self.update_bands, ["band_type", "num_bands"])
+        self.observe(self.update_fill, "fill")
+
+        self.loops.on_legend_click(self.on_legend_click)
+        self.loops.on_background_click(self.reset)
 
         self.update_bands(None)
         self.update_data(None)
@@ -95,19 +105,18 @@ class Radar(Figure):
             self.data_range[0], self.data_range[1], self.num_bands + 1
         )
         self.scaled_band_data = (
-            (band_data - self.data_range[0])
-            / (self.data_range[1] - self.data_range[0])
+            (band_data - self.data_range[0]) / (self.data_range[1] - self.data_range[0])
         )[:, np.newaxis]
 
         n = len(self.data.index)
 
-        if self.type == "circle":
+        if self.band_type == "circle":
             t = np.linspace(0, 2 * np.pi, 1000)
             band_data_x, band_data_y = (
                 self.scaled_band_data * np.cos(t),
                 self.scaled_band_data * np.sin(t),
             )
-        elif self.type == "polygon":
+        elif self.band_type == "polygon":
             t = np.linspace(0, 2 * np.pi, n + 1)
             band_data_x, band_data_y = (
                 self.scaled_band_data * np.sin(t),
@@ -119,9 +128,9 @@ class Radar(Figure):
             self.bands.y = band_data_y
 
         with self.band_labels.hold_sync():
-            self.band_labels.x = [0.0] * (self.num_bands + 1)
-            self.band_labels.y = self.scaled_band_data[:, 0]
-            self.band_labels.text = ["{:.2f}".format(b) for b in band_data]
+            self.band_labels.x = self.scaled_band_data[:, 0]
+            self.band_labels.y = [0.0] * (self.num_bands + 1)
+            self.band_labels.text = ["{:.0%}".format(b) for b in band_data]
 
     def update_data(self, *args):
         self.update_bands(None)
@@ -135,7 +144,7 @@ class Radar(Figure):
             np.cos(self.spoke_data_t),
         )
 
-        # Update marks based on data changes
+        # Update mark data based on data changes
         with self.spokes.hold_sync():
             self.spokes.x = np.column_stack(
                 [self.scaled_band_data[1] * spoke_data_x, spoke_data_x]
@@ -150,9 +159,44 @@ class Radar(Figure):
         data_x = scaled_data * np.sin(self.spoke_data_t)[:, np.newaxis]
         data_y = scaled_data * np.cos(self.spoke_data_t)[:, np.newaxis]
 
-        # update loops
+        # update data lines
         with self.loops.hold_sync():
             self.loops.x = np.column_stack([data_x.T, data_x.T[:, 0]])
             self.loops.y = np.column_stack([data_y.T, data_y.T[:, 0]])
-            self.loops.fill_opacities = [0.1] * len(self.loops.y)
+            if self.fill:
+                self.loops.fill = "inside"
+                self.loops.fill_opacities = [0.2] * len(self.loops.y)
+            else:
+                self.loops.fill = "none"
+                self.loops.fill_opacities = [0.0] * len(self.loops.y)
             self.loops.labels = [str(c) for c in self.data.columns]
+
+        # update spoke labels
+        t = np.linspace(0, 2 * np.pi, n + 1)
+        with self.spoke_labels.hold_sync():
+            self.spoke_labels.text = [str(row) for row in rows]
+            self.spoke_labels.x = np.sin(t)
+            self.spoke_labels.y = np.cos(t)
+
+    def update_fill(self, *args):
+        if self.fill == True:
+            with self.loops.hold_sync():
+                self.loops.fill = "inside"
+                self.loops.fill_opacities = [0.2] * len(self.loops.y)
+        else:
+            self.loops.fill = "none"
+            self.loops.fill_opacities = [0.0] * len(self.loops.y)
+
+    def on_legend_click(self, line, target):
+        selected_ix = target["data"]["index"]
+        n = len(line.y)
+        opacities = line.opacities
+        if opacities is None or len(opacities) == 0:
+            opacities = [1.0] * n
+
+        new_opacities = [0.1] * n
+        new_opacities[selected_ix] = 1
+        line.opacities = new_opacities
+
+    def reset(self, line, target):
+        line.opacities = [1.0] * len(line.y)
