8aae5cbbece5d660b8d30b5c56d2006564a464b6
martinRenou <martin.renou@gmail.com>
2023-07-17 16:38:12 +0200

    Try and uncomment tests

--- js/src/test/figure.ts
+++ js/src/test/figure.ts
@@ -8,32 +8,32 @@ describe('figure >', () => {
     this.manager = new DummyManager({ bqplot: bqplot });
   });
 
-  // it('marks removed before created', async function () {
-  //   const x = {
-  //     dtype: 'float32',
-  //     value: new DataView(new Float32Array([0.5, 0.5]).buffer),
-  //   };
-  //   const y = {
-  //     dtype: 'float32',
-  //     value: new DataView(new Float32Array([2.0, 2.5]).buffer),
-  //   };
-  //   const { scatter, figure } = await create_figure_scatter(this.manager, x, y);
+  it('marks removed before created', async function () {
+    const x = {
+      dtype: 'float32',
+      value: new DataView(new Float32Array([0.5, 0.5]).buffer),
+    };
+    const y = {
+      dtype: 'float32',
+      value: new DataView(new Float32Array([2.0, 2.5]).buffer),
+    };
+    const { scatter, figure } = await create_figure_scatter(this.manager, x, y);
 
-  //   // we start with the scatter and legend
-  //   expect(figure.fig_marks.node().children).lengthOf(2);
-  //   // we remove the scatter
-  //   figure.model.set('marks', []);
-  //   await Promise.all(figure.mark_views.views);
-  //   expect(figure.fig_marks.node().children).lengthOf(1);
-  //   // we set the marks twice without waiting, so we should never see the second scatter in the DOM
-  //   figure.model.set('marks', [scatter.model, scatter.model]);
-  //   const previousViewsPromise = Promise.all(figure.mark_views.views);
-  //   figure.model.set('marks', [scatter.model]);
-  //   await Promise.all(figure.mark_views.views);
-  //   await previousViewsPromise; // we also want to wait for these promises to be resolved so the dummy
-  //   // DOM node van be removed in the remove event handler in Figure.add_mark
-  //   expect(figure.fig_marks.node().children).lengthOf(2);
-  // });
+    // we start with the scatter and legend
+    expect(figure.fig_marks.node().children).lengthOf(2);
+    // we remove the scatter
+    figure.model.set('marks', []);
+    await Promise.all(figure.mark_views.views);
+    expect(figure.fig_marks.node().children).lengthOf(1);
+    // we set the marks twice without waiting, so we should never see the second scatter in the DOM
+    figure.model.set('marks', [scatter.model, scatter.model]);
+    const previousViewsPromise = Promise.all(figure.mark_views.views);
+    figure.model.set('marks', [scatter.model]);
+    await Promise.all(figure.mark_views.views);
+    await previousViewsPromise; // we also want to wait for these promises to be resolved so the dummy
+    // DOM node van be removed in the remove event handler in Figure.add_mark
+    expect(figure.fig_marks.node().children).lengthOf(2);
+  });
 
   it('min aspect check', async function () {
     const data_x = {
@@ -68,21 +68,21 @@ describe('figure >', () => {
     expect(x).to.be.equals(0);
     expect(y).to.be.equals(150);
 
-    // // relaxing it, we expect it to grow again
-    // figure.model.set('min_aspect_ratio', 1.0);
-    // ({width, height, x, y} = figure.getFigureSize());
-    // expect(width).to.be.equals(400);
-    // expect(height).to.be.equals(400);
-    // expect(x).to.be.equals(0);
-    // expect(y).to.be.equals(50);
+    // relaxing it, we expect it to grow again
+    figure.model.set('min_aspect_ratio', 1.0);
+    ({width, height, x, y} = figure.getFigureSize());
+    expect(width).to.be.equals(400);
+    expect(height).to.be.equals(400);
+    expect(x).to.be.equals(0);
+    expect(y).to.be.equals(50);
 
-    // // and back to its original size
-    // figure.model.set('min_aspect_ratio', 0.1);
-    // ({width, height, x, y} = figure.getFigureSize());
-    // expect(width).to.be.equals(400);
-    // expect(height).to.be.equals(500);
-    // expect(x).to.be.equals(0);
-    // expect(y).to.be.equals(0);
+    // and back to its original size
+    figure.model.set('min_aspect_ratio', 0.1);
+    ({width, height, x, y} = figure.getFigureSize());
+    expect(width).to.be.equals(400);
+    expect(height).to.be.equals(500);
+    expect(x).to.be.equals(0);
+    expect(y).to.be.equals(0);
   });
 
   it('title respect', async function () {
